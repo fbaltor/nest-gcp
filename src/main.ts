81d0d21b395c67639aa211153c5622c0f807f24e
Felipe Baltor <fbaltor@gmail.com>
2021-06-13 23:53:18 -0300

    fix: main.ts PORT variable set

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+let PORT = process.env.PORT ? +process.env.PORT : 9999;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
